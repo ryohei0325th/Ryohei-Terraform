905f03b68949fdd8d2a6d685db55112d108f54d3
ryohei0325th <ryohei0325th@gmail.com>
2022-07-05 18:09:32 +0900

    Terraform

---
+++ terraform/variable.tf
@@ -0,0 +1,8 @@
+variable "tag-name" {
+  default = "terraform-suzuki"
+}
+
+variable "my-ip" {
+  default = "219.115.28.220/32"
+}
+
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,7 @@
 resource "aws_vpc" "suzuki-vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.tag-name}-vpc"
+  }
 }
